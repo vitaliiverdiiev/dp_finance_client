4aaa83217c628e9c018857b4ab7ae8eacf9d496e
vitaliiverdiiev <vitaliiverdiiev@gmail.com>
2024-12-06 14:35:28 +0200

    add: Helmet

--- src/pages/shopping-list/ui/shopping-list.page.tsx
+++ src/pages/shopping-list/ui/shopping-list.page.tsx
@@ -4,6 +4,7 @@ import { GET_SHOPPING_LIST_ITEMS } from '../../../entities/shopping-list/schemas
 import { Error, PageTitle } from '@/shared/ui';
 import { ShoppingList } from '@/widgets/shopping-list/ui/shopping-list';
 import { ShoppingListItemSkeleton } from '@/widgets/shopping-list/ui/shopping-list-item.skeleton';
+import { Helmet } from 'react-helmet';
 
 export const ShoppingListPage = () => {
   const { loading, error, data } = useQuery(GET_SHOPPING_LIST_ITEMS);
@@ -14,19 +15,26 @@ export const ShoppingListPage = () => {
   console.log({ data });
 
   return (
-    <section>
-      <div className="container">
-        <PageTitle title="Shopping list" />
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
-          <div>
-            {loading && <ShoppingListItemSkeleton />}
-            {data && <ShoppingList items={data?.shoppingList} />}
-          </div>
-          <div className="space-y-4">
-            <ShoppingListForm />
+    <>
+      <Helmet>
+        <title>Shopping list</title>
+        <meta name="description" content="Shopping list page"/>
+        <meta name="keywords" content="shopping list,personal fiance"/>
+      </Helmet>
+      <section>
+        <div className="container">
+          <PageTitle title="Shopping list" />
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+            <div>
+              {loading && <ShoppingListItemSkeleton />}
+              {data && <ShoppingList items={data?.shoppingList} />}
+            </div>
+            <div className="space-y-4">
+              <ShoppingListForm />
+            </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </> 
   );
 };
